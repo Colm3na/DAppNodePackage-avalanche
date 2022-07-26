7ce771a87649a3569514a05358fc928e365cd55b
Eduardo A. Diez <eduadiez@gmail.com>
2022-07-27 00:09:56 +0200

    add dappnode to constants and to network

--- build-ui/constants.ts
+++ build-ui/constants.ts
@@ -1,19 +1,28 @@
-import { AvaNetwork } from "@/js/AvaNetwork";
+import { AvaNetwork } from '@/js/AvaNetwork'
 
 export const MainnetConfig = new AvaNetwork(
-  "Mainnet dappnode",
-  "http://avalanche.avalanche.public.dappnode:9650",
-  1,
-  "https://explorerapi.avax.network",
-  "https://explorer-xp.avax.network",
-  true
-);
+    'Mainnet',
+    'https://api.avax.network:443',
+    1,
+    'https://explorerapi.avax.network',
+    'https://explorer-xp.avax.network',
+    true
+)
 
 export const TestnetConfig = new AvaNetwork(
-  "Fuji",
-  "https://api.avax-test.network:443",
-  5,
-  "https://explorerapi.avax-test.network",
-  "https://explorer-xp.avax-test.network",
-  true
-);
+    'Fuji',
+    'https://api.avax-test.network:443',
+    5,
+    'https://explorerapi.avax-test.network',
+    'https://explorer-xp.avax-test.network',
+    true
+)
+
+export const DappnodeConfig = new AvaNetwork(
+    'DAppNode Mainnet',
+    'http://avalanche.avalanche.public.dappnode:9650',
+    1,
+    'https://explorerapi.avax.network',
+    'https://explorer.avax.network',
+    true
+)
